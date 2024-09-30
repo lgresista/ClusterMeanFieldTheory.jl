5892bbdf83be46af4191b35e4783cc98dae74df2
lgresist <gresista@thp.uni-koeln.de>
2024-09-30 15:50:11 +0200

    update get_meanfield_cluster

--- src/ClusterMeanFieldTheory.jl
+++ src/ClusterMeanFieldTheory.jl
@@ -24,7 +24,7 @@ export MeanFieldCluster
 export nsites, magnetic_fields
 export set_magnetizations!, recalculate_magnetic_fields!
 export calculate_groundstate_energy, calculate_energyshift
-export get_meanfield_cluster_interactions
+export get_meanfield_cluster_interactions, get_meanfield_cluster, calculate_nsites
 export fixedpoint_iteration!, anderson_acceleration!
 
 end
