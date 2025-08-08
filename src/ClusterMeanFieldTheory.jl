d72d9641c2881f3cc88f1d2d2cbda779cfa5a163
Lasse Gresista <gresista@thp.uni-koeln.de>
2025-08-08 12:55:29 +0200

    add readme file

--- src/ClusterMeanFieldTheory.jl
+++ src/ClusterMeanFieldTheory.jl
@@ -28,6 +28,7 @@ export calculate_groundstate_energy, calculate_energyshift, calculate_groundstat
 export fixedpoint_iteration!, anderson_acceleration!
 
 # help cluster initialization
+export UnitCell
 export LabeledBond, get_bond, get_label
 export get_meanfield_cluster_interactions, get_meanfield_cluster, calculate_nsites
 export get_unitcell_3x, get_bonds_3x, loc_to_loc3x
